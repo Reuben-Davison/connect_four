c1977da81101e12263f376b963cb499cba031f1b
reuben <creubend@gmail.com>
2022-02-07 19:13:09 -0700

    test: Add rows and columns test

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,10 @@
 class Board
+  attr_reader :rows, :columns
+  
+  
+  def initialize(rows = 6, columns = 7)
+    @rows = rows
+    @columns = columns
+  end
+  
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Board do
     board = Board.new
     expect(board).to be_an_instance_of(Board)
   end
+  
+  
+  it "has rows and columns" do
+    board = Board.new
+    expect(board.rows).to eq(6)
+    expect(board.columns).to eq(7)
+    
+  end
+  
+  
+  
+  
 end
