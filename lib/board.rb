f7783350d0bdda057cd64f262ac2761ec6e131a6
Luke Pascale <lpluke5@gmail.com>
2022-02-08 16:56:51 -0700

    feat: Add initial steps to rendering the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,5 @@
 class Board
-  attr_reader :rows, :columns, :divs, :rows_hash
-
+  attr_reader :rows, :columns, :divs, :rows_hash, :render
 
   def initialize(rows = 6, columns = 7)
     @rows = rows
@@ -14,9 +13,12 @@ class Board
       "row2" => [".", ".", ".", ".", ".", ".", "."],
       "row1" => [".", ".", ".", ".", ".", ".", "."],
     }
+    # new_board = "A B C D E F G\n#{rows_hash["row6"][0]} #{rows_hash["row6"][1]} #{rows_hash["row6"][2]} #{rows_hash["row6"][3]} #{rows_hash["row6"][4]} #{rows_hash["row6"][5]} #{rows_hash["row6"][6]} \n#{rows_hash["row5"][0]} #{rows_hash["row5"][1]} #{rows_hash["row5"][2]} #{rows_hash["row5"][3]} #{rows_hash["row5"][4]} #{rows_hash["row5"][5]} #{rows_hash["row5"][6]} \n#{rows_hash["row4"][0]} #{rows_hash["row4"][1]} #{rows_hash["row4"][2]} #{rows_hash["row4"][3]} #{rows_hash["row4"][4]} #{rows_hash["row4"][5]} #{rows_hash["row4"][6]} \n#{rows_hash["row3"][0]} #{rows_hash["row3"][1]} #{rows_hash["row3"][2]} #{rows_hash["row3"][3]} #{rows_hash["row3"][4]} #{rows_hash["row3"][5]} #{rows_hash["row3"][6]} \n#{rows_hash["row2"][0]} #{rows_hash["row2"][1]} #{rows_hash["row2"][2]} #{rows_hash["row2"][3]} #{rows_hash["row2"][4]} #{rows_hash["row2"][5]} #{rows_hash["row2"][6]} \n#{rows_hash["row1"][0]} #{rows_hash["row1"][1]} #{rows_hash["row1"][2]} #{rows_hash["row1"][3]} #{rows_hash["row1"][4]} #{rows_hash["row1"][5]} #{rows_hash["row1"][6]}\n"
   end
 
+
   def render
-    print "ABCDEFG \n#{rows_hash["row6"]} \n#{rows_hash["row6"]} \n#{rows_hash["row5"]} \n#{rows_hash["row4"]} \n#{rows_hash["row3"]} \n#{rows_hash["row2"]} \n#{rows_hash["row1"]}"
+    new_board = "A B C D E F G\n#{rows_hash["row6"][0]} #{rows_hash["row6"][1]} #{rows_hash["row6"][2]} #{rows_hash["row6"][3]} #{rows_hash["row6"][4]} #{rows_hash["row6"][5]} #{rows_hash["row6"][6]} \n#{rows_hash["row5"][0]} #{rows_hash["row5"][1]} #{rows_hash["row5"][2]} #{rows_hash["row5"][3]} #{rows_hash["row5"][4]} #{rows_hash["row5"][5]} #{rows_hash["row5"][6]} \n#{rows_hash["row4"][0]} #{rows_hash["row4"][1]} #{rows_hash["row4"][2]} #{rows_hash["row4"][3]} #{rows_hash["row4"][4]} #{rows_hash["row4"][5]} #{rows_hash["row4"][6]} \n#{rows_hash["row3"][0]} #{rows_hash["row3"][1]} #{rows_hash["row3"][2]} #{rows_hash["row3"][3]} #{rows_hash["row3"][4]} #{rows_hash["row3"][5]} #{rows_hash["row3"][6]} \n#{rows_hash["row2"][0]} #{rows_hash["row2"][1]} #{rows_hash["row2"][2]} #{rows_hash["row2"][3]} #{rows_hash["row2"][4]} #{rows_hash["row2"][5]} #{rows_hash["row2"][6]} \n#{rows_hash["row1"][0]} #{rows_hash["row1"][1]} #{rows_hash["row1"][2]} #{rows_hash["row1"][3]} #{rows_hash["row1"][4]} #{rows_hash["row1"][5]} #{rows_hash["row1"][6]}\n"
+    print new_board
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,31 +2,30 @@ require "pry"
 require "rspec"
 require "./lib/board"
 RSpec.describe Board do
-  it "exists" do
-    board = Board.new
-    expect(board).to be_an_instance_of(Board)
+  describe '#iteration 1' do
+    it "exists" do
+      board = Board.new
+
+      expect(board).to be_an_instance_of(Board)
+    end
+
+    it "has rows and columns" do
+      board = Board.new
+
+      expect(board.rows).to eq(6)
+      expect(board.columns).to eq(7)
+    end
+
+    it "can read each div" do
+      board = Board.new
+
+      expect(board.divs).to be_a(Hash)
+    end
+
+    it "can render a board" do
+      board = Board.new
+  binding.pry 
+      expect(board.render).to eq("A B C D E F G \n. . . . . . . \n. . . . . . . \n. . . . . . . \n. . . . . . . \n. . . . . . . \n. . . . . . . \n. . . . . . .")
+    end
   end
-
-
-  it "has rows and columns" do
-    board = Board.new
-    expect(board.rows).to eq(6)
-    expect(board.columns).to eq(7)
-
-  end
-
-
-  it "can read each div" do
-    board = Board.new
-    expect(board.divs).to be_a(Hash)
-
-  end
-
-  it "can render a board" do
-    board = Board.new
-    binding.pry
-    expect(board.render).to eq("ABCDEFG \n#{board.rows_hash["row6"]}   \n#{board.rows_hash["row6"]} \n#{board.rows_hash["row5"]} \n#{board.rows_hash["row4"]} \n#{board.rows_hash["row3"]} \n#{board.rows_hash["row2"]} \n#{board.rows_hash["row1"]}")
-
-  end
-
 end
