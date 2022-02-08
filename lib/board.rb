6c030fe19765dbc9302d497b6fbdce9fda6971c2
Luke Pascale <lpluke5@gmail.com>
2022-02-07 20:58:02 -0700

    test: Add testing for board.render

--- lib/board.rb
+++ lib/board.rb
@@ -1,26 +1,22 @@
 class Board
   attr_reader :rows, :columns, :divs, :rows_hash
-  
-  
+
+
   def initialize(rows = 6, columns = 7)
     @rows = rows
     @columns = columns
     @divs = {}
-    @rows_hash = { 
+    @rows_hash = {
       "row6" => [".", ".", ".", ".", ".", ".", "."],
       "row5" => [".", ".", ".", ".", ".", ".", "."],
       "row4" => [".", ".", ".", ".", ".", ".", "."],
       "row3" => [".", ".", ".", ".", ".", ".", "."],
       "row2" => [".", ".", ".", ".", ".", ".", "."],
       "row1" => [".", ".", ".", ".", ".", ".", "."],
-
     }
   end
-  
+
   def render
-    
-  
-  # binding.pry
-  return "ABCDEFG \n#{rows_hash["row6"]}"
+    print "ABCDEFG \n#{rows_hash["row6"]} \n#{rows_hash["row6"]} \n#{rows_hash["row5"]} \n#{rows_hash["row4"]} \n#{rows_hash["row3"]} \n#{rows_hash["row2"]} \n#{rows_hash["row1"]}"
   end
 end
