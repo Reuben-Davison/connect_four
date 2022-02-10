fa1ca19b851e6bca0a5689dbd65ac9e088b5ac69
Luke Pascale <lpluke5@gmail.com>
2022-02-09 18:58:40 -0700

    test: Add more testing for tokens used by  Div class

--- lib/div.rb
+++ lib/div.rb
@@ -1,9 +1,15 @@
-class Div
 # going to tell the program, what token is currently in the slot. each div will have a significant number/lettter associated so that values can be checked for winner.
+class Div
+  attr_reader :token,
+              :ply1_token,
+              :ply2_token,
+              :comp_token
+
   def initialize(token)
     @token = "."
-    @player1_token = "X"
-    @player2_token = "O"
-    @computer = "O"
+    @ply1_token = "X"
+    @ply2_token = "O"
+    @comp_token = "O"
   end
+
 end
--- spec/div_spec.rb
+++ spec/div_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe Div do
 
       expect(div1).to be_a(Div)
     end
+
+    it 'can read the tokens' do
+      div1 = Div.new(".")
+
+      expect(div1.token).to eq(".")
+      expect(div1.ply1_token).to eq("X")
+      expect(div1.ply2_token).to eq("O")
+      expect(div1.comp_token).to eq("O")
+    end
   end
 end
