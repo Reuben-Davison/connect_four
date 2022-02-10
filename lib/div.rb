b231acb278a36c3c529056e0bf3cad0488b612d3
Luke Pascale <lpluke5@gmail.com>
2022-02-09 18:51:39 -0700

    test: Begin testing pattern for div spec

--- lib/div.rb
+++ lib/div.rb
@@ -1,7 +1,9 @@
-
 class Div
-# going to tell the program, what token is currently in the slot. each div will have a significant number/lettter associated so that values can be checked for winner. 
-  def initialize(player1_token, player2_token, blank)
-    @player1_token = ""
+# going to tell the program, what token is currently in the slot. each div will have a significant number/lettter associated so that values can be checked for winner.
+  def initialize(token)
+    @token = "."
+    @player1_token = "X"
+    @player2_token = "O"
+    @computer = "O"
   end
 end
--- spec/div_spec.rb
+++ spec/div_spec.rb
@@ -2,3 +2,13 @@ require "pry"
 require "rspec"
 require "./lib/board"
 require "./lib/div"
+
+RSpec.describe Div do
+  context 'create div class' do
+    it 'exists' do
+      div1 = Div.new(".")
+
+      expect(div1).to be_a(Div)
+    end
+  end
+end
